b637d2eafb94d9c502cdd00ce2df05f5e13a6697
Rodrigo Borges <rodrigocborges@hotmail.com>
2021-11-17 09:21:22 -0300

    Tamanho da janela definido e teste de iluminação

--- base_runner.py
+++ base_runner.py
@@ -14,17 +14,48 @@ import numpy as np
 global time_control
 time_control = 0
 
-
 def create_scene(time,):
     global time_control
     time_control = time_control + 1
-    glutTimerFunc(2000, create_scene, 0)
+    glutTimerFunc(1000, create_scene, 0)
     glutPostRedisplay()
+
+def lights():
+    ambient_intensity = [1, 1, 1, 1.0]
+
+    direction = [0.0, 45.0, -1.0, 1.0]
+
+    intensity = [1, 0.7, 1, 1.0]
+
+    #Habilitar iluminação
+    glEnable(GL_LIGHTING)
+
+    #Definir o modelo de iluminação
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient_intensity)
+
+    #Habilitar iluminação 0
+    glEnable(GL_LIGHT0)
+
+    # Definindo posição e intensidade da luz
+    glLightfv(GL_LIGHT0, GL_POSITION, direction)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, intensity)
+
+    # Configurando o material
+    glEnable(GL_COLOR_MATERIAL)
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
+    glMaterialf(GL_FRONT, GL_SHININESS, 5.0)
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, [0.1, 0.1, 0.1, 1.0])
 
 def display():
     # Clear the color and depth buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    #Habilitar o Z-buffer
     glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glDisable(GL_CULL_FACE)
+    glCullFace(GL_BACK)
+
     glEnable(GL_FOG)
     glPushMatrix()
     glScalef(0.15, 0.15, 0.15)
@@ -33,10 +64,12 @@ def display():
     position_y = random.randint(0, 5)
     global time_control
     if time_control < 5 or time_control > 15:
+        glDisable(GL_LIGHTING)
         DrawSymbol()
     else:
         print(angle)
         glRotate(angle, 0, 1, 0)
+        lights()
         DrawMorrinho()
 
     glPopMatrix()
@@ -52,6 +85,7 @@ glutInit(sys.argv)
 glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
 
 # Create a window, setting its title
+glutInitWindowSize(800, 500)
 glutCreateWindow('FURG - Rodrigo e Gloria')
 
 # Set the display callback.  You can set other callbacks for keyboard and
